2aca343e14a5d115cf71050c0f3a3cf00b581896
chall3n <chall3n@gmail.com>
2025-08-07 22:25:18 -0400

    Refactoring waveformplayer to stick to Tldraw whiteboard and scale the same as other objects

---
+++ src/components/WaveformShape.tsx
@@ -0,0 +1,53 @@
+
+import { BaseBoxShapeUtil, TLBaseShape, Tldraw } from 'tldraw'
+
+// A type for our custom shape
+export type WaveformShape = TLBaseShape<
+	'waveform',
+	{
+		w: number
+		h: number
+		assetId: string | null
+	}
+>
+
+// A class for our custom shape's utility
+export class WaveformShapeUtil extends BaseBoxShapeUtil<WaveformShape> {
+	static override type = 'waveform' as const
+
+	override canResize = (shape: WaveformShape) => true
+	override canBind = (shape: WaveformShape) => true
+
+	// Default props
+	override getDefaultProps(): WaveformShape['props'] {
+		return {
+			w: 300,
+			h: 120,
+			assetId: null,
+		}
+	}
+
+	// Render method
+	override render(shape: WaveformShape) {
+		return (
+			<div
+				style={{
+					width: shape.props.w,
+					height: shape.props.h,
+					backgroundColor: 'lightblue',
+					border: '1px solid black',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+				}}
+			>
+				<p>Waveform Player Placeholder</p>
+			</div>
+		)
+	}
+
+	// Indicator
+	override indicator(shape: WaveformShape) {
+		return <rect width={shape.props.w} height={shape.props.h} />
+	}
+}
