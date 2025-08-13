4ed1f71fa5718a2268d6f8952e02e92c633720a3
chall3n <chall3n@gmail.com>
2025-08-12 22:49:41 -0400

    Added auth

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -5,6 +5,7 @@ import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
 
+//auth page
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
