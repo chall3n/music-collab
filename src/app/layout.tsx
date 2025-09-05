9c03a01a45690c52789e5d31f42f2796182df96d
chall3n <chall3n@gmail.com>
2025-09-04 23:36:46 -0400

    Changed font to Rokkitt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rokkitt } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const rokkitt = Rokkitt({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-rokkitt",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${rokkitt.variable} font-sans antialiased`}>{children}</body>
     </html>
   );
 }
